56b19a8f44b7fb779cae5ce2da63387949231eb9
Tomas Jonuska <tommasavas@gmail.com>
2022-07-14 22:49:24 +0300

    models updated

--- addboard/addboard_site/models.py
+++ addboard/addboard_site/models.py
@@ -1,3 +1,17 @@
+from tabnanny import verbose
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_('name'), max_length=100, db_index=True)
+    priority = models.IntegerField(_('priority'), default = 0, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+        ordering = ('-priority', 'name')
+
